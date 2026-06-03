951f5cd1eb5de28203076441acd90c5e3379d81b
jeenatamang <lamajeena89@gmail.com>
2026-06-03 22:15:45 +0545

    feat: day 3 - built native http server

---
+++ day3.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+// create the server instance
+const server = http.createServer((req, res) => {
+    // set HTTP status and content headers
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello there!');
+});
+
+// make the server listen on port 3000
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
