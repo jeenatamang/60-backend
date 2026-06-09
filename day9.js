031136c7f06b48cc23516dd6f8d13deb505aef73
jeenatamang <lamajeena89@gmail.com>
2026-06-09 16:46:21 +0545

    feat: day 9 implemented simple express get and post routes

---
+++ day9.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.get('/user', (req, res) => {
+    res.send('fetching user profile data...');
+});
+app.post('/user', (req, res) => {
+    console.log('received body:', req.body);
+    res.send('user data processed');
+});
+app.listen(3000, () => {
+    console.log('server active on 3000...');
+});
+
+
