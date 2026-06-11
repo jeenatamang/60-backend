34092df994dacfec86f09d96ed89e474f6288a42
jeenatamang <lamajeena89@gmail.com>
2026-06-11 15:34:22 +0545

    feat: day 11 - added simple request logging middleware

---
+++ day11.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(`Request type: ${req.method} to ${req.url}`);
+
+    next(); 
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome page');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.send('Dashboard page');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on 3000');
+});
