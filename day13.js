7eed16fc02f91fb2729fe2805287eb86c7fe6a71
jeenatamang <lamajeena89@gmail.com>
2026-06-13 21:51:09 +0545

    feat: day 13 implemented global error handling middleware

---
+++ day13.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the homepage');
+});
+app.get('/broken-route', (req, res, next) => {
+    const systemError = new Error('Database connection failed!');    
+    next(systemError); 
+});
+
+// Must have exactly 4 arguments: err, req, res, next
+app.use((err, req, res, next) => {
+    console.error('SERVER LOG [ALERT]:', err.message);
+
+    res.status(500).send('Something went wrong. Please try again later.');
+});
+app.listen(3000, () => {
+    console.log('Error-secure server running on port 3000');
+});
+
+
+
