0c55d7b16da892e4d10f54aace7c90ac796a5069
jeenatamang <lamajeena89@gmail.com>
2026-06-15 22:59:24 +0545

    feat: day 15 set up file-based data storage

---
+++ day15.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs'); 
+const app = express();
+
+app.use(express.json());
+const DATA_FILE = './database.json';
+app.post('/save', (req, res) => {
+    const newData = req.body;
+
+    fs.writeFileSync(DATA_FILE, JSON.stringify(newData, null, 2));
+    
+    res.send('Data safely saved to the database file!');
+});
+app.listen(3000, () => {
+    console.log('Database server active on port 3000');
+});
+
+
